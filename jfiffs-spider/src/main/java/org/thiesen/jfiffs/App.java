751f99a2cfd9c494d777804e3c30847100d32b9a
Marcus Thiesen <marcus@thiesen.org>
2018-10-25 09:34:44 +0200

    Initial

---
+++ jfiffs-spider/src/main/java/org/thiesen/jfiffs/App.java
@@ -0,0 +1,13 @@
+package org.thiesen.jfiffs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
